0b83df68fc6f222f87e32ebcb157d351e18cc1e9
dev-sereti <devsereti@gmail.com>
2025-10-02 09:29:32 +0300

    maxLength changed

--- Learn Js/Restricted Textarea/script.js
+++ Learn Js/Restricted Textarea/script.js
@@ -1,6 +1,6 @@
 const textArea=document.getElementById("message");
 const counter =document.getElementById("counter");
-const maxLenght=300;
+const maxLenght=250;
 
 textArea.addEventListener("input",()=>{
     let currentLength=textArea.ariaValueMax.length;
