bfad3af6b3e5de16d181fbcc0827c64f64670697
dev-sereti <devsereti@gmail.com>
2025-10-02 08:52:35 +0300

    Throw error if birthdate is empty

--- Learn Js/AgeCalculator/script.js
+++ Learn Js/AgeCalculator/script.js
@@ -1,4 +1,8 @@
 document.getElementById("calculateBtn").addEventListener("click",()=>{
   const birthDate= document.getElementById("birthdate");
   const result=document.getElementById("result");
+
+  if(!birthDate){
+    result.textContent="Please pick your date of birth"
+  }
 });
