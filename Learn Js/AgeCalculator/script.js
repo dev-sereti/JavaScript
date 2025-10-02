1e8aa1a9d19d3b6eca35e8d9d47b878c6c566dfe
dev-sereti <devsereti@gmail.com>
2025-10-02 08:49:54 +0300

    get result from html

--- Learn Js/AgeCalculator/script.js
+++ Learn Js/AgeCalculator/script.js
@@ -1,3 +1,4 @@
 document.getElementById("calculateBtn").addEventListener("click",()=>{
   const birthDate= document.getElementById("birthdate");
+  const result=document.getElementById("result");
 });
