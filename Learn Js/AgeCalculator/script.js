049747d6fb94a8c6baafea34a1ee47838cd30410
dev-sereti <devsereti@gmail.com>
2025-10-02 08:18:04 +0300

    Error if date not picked

--- Learn Js/AgeCalculator/script.js
+++ Learn Js/AgeCalculator/script.js
@@ -1,6 +1,9 @@
 document.getElementById("calculateBtn").addEventListener("click"),()=>{
   const birthDate=document.getElementById("birthdate").value;
   const result=document.getElementById("result");
-  
 
+  if(!birthDate){
+    result.textContent="Please select your date of birth";
+  }
+  
 });
