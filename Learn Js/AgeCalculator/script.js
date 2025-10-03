542ccb58523d4c6d3a03bb81b5298f336d1907c1
dev-sereti <devsereti@gmail.com>
2025-10-03 19:46:23 +0300

    Check if birthDate field is empty.

--- Learn Js/AgeCalculator/script.js
+++ Learn Js/AgeCalculator/script.js
@@ -1,4 +1,8 @@
 document.getElementById("calculateBtn").addEventListener("click",()=>{
   const birthDate=document.getElementById("birthdate").value;
   const age =document.getElementById("result");
+
+  if(!birthDate){
+    age.textContent="Select your birth date";
+  }
 });
