4ad0e116163c239dd2f2580b073e9de6cbf7bff5
dev-sereti <devsereti@gmail.com>
2025-10-03 19:44:10 +0300

    create a constant variable age to retrieve a result HTML element  value

--- Learn Js/AgeCalculator/script.js
+++ Learn Js/AgeCalculator/script.js
@@ -1,4 +1,4 @@
 document.getElementById("calculateBtn").addEventListener("click",()=>{
   const birthDate=document.getElementById("birthdate").value;
-  
+  const age =document.getElementById("result");
 });
