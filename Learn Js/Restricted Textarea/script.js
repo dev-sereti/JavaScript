ace3b150619c29e643d6d62d4186fd52ede9a4ce
dev-sereti <devsereti@gmail.com>
2025-10-04 10:22:57 +0300

    get textArea element

--- Learn Js/Restricted Textarea/script.js
+++ Learn Js/Restricted Textarea/script.js
@@ -1,18 +1,1 @@
-const textarea = document.getElementById("message");
-const counter = document.getElementById("counter");
-const maxLength = 250;
-
-textarea.addEventListener("input", () => {
-  let currentLength = textarea.value.length;
-
-  counter.textContent = `${currentLength} / ${maxLength}`;
-
-  if (currentLength >= maxLength) {
-    textarea.classList.add("limit");
-    counter.classList.add("limit");
-    textarea.value = textarea.value.substring(0, maxLength); // Prevent extra input
-  } else {
-    textarea.classList.remove("limit");
-    counter.classList.remove("limit");
-  }
-});
+const textArea=document.getElementById("message");
