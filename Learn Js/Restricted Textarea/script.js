0502c7fe97d32dcb0ebb7a6b8a9d96d0d5fdba7b
dev-sereti <devsereti@gmail.com>
2025-10-04 16:54:18 +0300

    current length= textArea.

--- Learn Js/Restricted Textarea/script.js
+++ Learn Js/Restricted Textarea/script.js
@@ -3,5 +3,17 @@ const counter=document.getElementById("counter");
 const maxLength=400;
 
 textArea.addEventListener("input",()=>{
+  let currentLength=textArea.value.maxLength;
+
+  counter= `${currentLength} / ${maxLength}`;
+
+  if(currentLength>=maxLength){
+    textArea.classList.add("limit");
+    counter.classList.add("limit");
+
+  }else{
+    textArea.classList.remove("limit");
+    counter.classList.remove("limit");
+  }
 
 });
