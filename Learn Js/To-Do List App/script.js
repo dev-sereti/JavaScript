5bf4e6146cd795fe3676cc8edc66a90a74915f42
dev-sereti <devsereti@gmail.com>
2025-10-04 17:17:00 +0300

    Load tasks from localStorage on page load

--- Learn Js/To-Do List App/script.js
+++ Learn Js/To-Do List App/script.js
@@ -1,3 +1,7 @@
 const taskInput=document.getElementById("taskInput");
 const addBtn=document.getElementById("addBtn");
 const taskList=document.getElementById("taskList");
+
+// Load tasks from localStorage on page load
+window.onload=loadTasks;
+
