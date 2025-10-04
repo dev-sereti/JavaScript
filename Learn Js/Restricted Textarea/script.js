e5417d324b85173f53b83d676d1e34947678e496
dev-sereti <devsereti@gmail.com>
2025-10-04 11:04:10 +0300

    else statement

--- Learn Js/Restricted Textarea/script.js
+++ Learn Js/Restricted Textarea/script.js
@@ -6,15 +6,16 @@ const maxLength=400;
 // textArea input addEventListener
 
 textArea.addEventListener("input",()=>{
-  let currentCounter=textArea.ariaValueMax.maxLength;
-  counter=`${currentCounter}/${maxLength}`;
+  let currentLength=textArea.value.maxLength;
+  counter.textContent=`${currentLength}/${maxLength}`;
 
-  if(currentCounter>=maxLength){
+  if(currentLength>=maxLength){
     textArea.classlist.add("limit");
     counter.classList.add("limit");
     textArea.value=textArea.value.substring(0,maxLength);
   }else{
-    
+    textArea.classList.remove("limit");
+    counter.classList.remove("limit");
   }
 
 
