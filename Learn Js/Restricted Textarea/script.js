c8a989d2bffe4695b6284b242c29eabaf91122aa
dev-sereti <devsereti@gmail.com>
2025-10-04 11:09:16 +0300

    limit updated

--- Learn Js/Restricted Textarea/script.js
+++ Learn Js/Restricted Textarea/script.js
@@ -6,13 +6,13 @@ const maxLength=400;
 // textArea input addEventListener
 
 textArea.addEventListener("input",()=>{
-  let currentLength=textArea.value.maxLength;
-  counter.textContent=`${currentLength}/${maxLength}`;
+  let currentLength=textArea.value.length;
+  counter.textContent=`${currentLength} / ${maxLength}`;
 
   if(currentLength>=maxLength){
     textArea.classlist.add("limit");
     counter.classList.add("limit");
-    textArea.value=textArea.value.substring(0,maxLength);
+    //textArea.value=textArea.value.substring(0,maxLength);
   }else{
     textArea.classList.remove("limit");
     counter.classList.remove("limit");
