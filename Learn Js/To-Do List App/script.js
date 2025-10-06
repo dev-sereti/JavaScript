255cfc9f9dcc4990a5be3a6e79c63aca5c0d52f5
dev-sereti <devsereti@gmail.com>
2025-10-06 15:21:46 +0300

    Check if the input is empty

--- Learn Js/To-Do List App/script.js
+++ Learn Js/To-Do List App/script.js
@@ -3,12 +3,16 @@ const addBtn = document.getElementById("addBtn");
 const taskList=document.getElementById("taskList");
 
 // Add Task
-
 function addTask(){
     const taskText=taskInput.value.trim();
+
+    //Check if the input is empty
     if(taskText==="")return;
 
     const li=createElement(taskText);
+    taskList.appendChild(li);
+    saveTask(taskText);
+    taskInput.value="";
     
 }
 
