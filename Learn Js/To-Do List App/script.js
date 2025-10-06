343413595d6e679ab6c219b63ec41547eb85acbd
dev-sereti <devsereti@gmail.com>
2025-10-06 15:12:58 +0300

    function addTask

--- Learn Js/To-Do List App/script.js
+++ Learn Js/To-Do List App/script.js
@@ -2,6 +2,11 @@ const taskInput=document.getElementById("taskInput");
 const addBtn = document.getElementById("addBtn");
 const taskList=document.getElementById("taskList");
 
+// Add Task
+
+function addTask(){
+    
+}
 
 // Create Element.
 function createElement(){
