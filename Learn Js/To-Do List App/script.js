ea1105ebbabf5052bde7dbf571fe8d60c06f683b
dev-sereti <devsereti@gmail.com>
2025-10-06 15:10:13 +0300

    Create element function

--- Learn Js/To-Do List App/script.js
+++ Learn Js/To-Do List App/script.js
@@ -1,3 +1,10 @@
 const taskInput=document.getElementById("taskInput");
 const addBtn = document.getElementById("addBtn");
 const taskList=document.getElementById("taskList");
+
+
+// Create Element.
+function createElement(){
+
+}
+
