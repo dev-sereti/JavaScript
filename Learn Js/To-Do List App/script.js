ba07e265e6498d92f5dadbcfe4e6dd30467c0723
dev-sereti <devsereti@gmail.com>
2025-10-06 15:22:43 +0300

    Add the task to the task list

--- Learn Js/To-Do List App/script.js
+++ Learn Js/To-Do List App/script.js
@@ -4,13 +4,15 @@ const taskList=document.getElementById("taskList");
 
 // Add Task
 function addTask(){
+    //Get the text from the input field
     const taskText=taskInput.value.trim();
 
     //Check if the input is empty
     if(taskText==="")return;
 
+    //Create a new task <li> element
     const li=createElement(taskText);
-    taskList.appendChild(li);
+    taskList.appendChild(li);//Add the task to the task list
     saveTask(taskText);
     taskInput.value="";
     
