128b4e1c23c99a6a99319bcbbac1413ba5ecb83e
dev-sereti <devsereti@gmail.com>
2025-10-07 09:15:55 +0300

    When the page loads, call loadTasks() to show saved tasks

--- Learn Js/To-Do List App/script.js
+++ Learn Js/To-Do List App/script.js
@@ -3,3 +3,8 @@ const taskInput=document.getElementById("taskInput");
 const addBtn=document.getElementById("addBtn");
 const taskList=document.getElementById("taskList");
 
+// When the page loads, call loadTasks() to show saved tasks
+window.onload=loadTasks;
+
+
+
