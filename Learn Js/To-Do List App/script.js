7c47f59d212d97d9ee2b6fd29b4af9d5b28f43da
dev-sereti <devsereti@gmail.com>
2025-10-07 09:12:48 +0300

    Get references to HTML elements by their ID

--- Learn Js/To-Do List App/script.js
+++ Learn Js/To-Do List App/script.js
@@ -1,1 +1,5 @@
 // Get references to HTML elements by their ID
+const taskInput=document.getElementById("taskInput");
+const addBtn=document.getElementById("addBtn");
+const taskList=document.getElementById("taskList");
+
