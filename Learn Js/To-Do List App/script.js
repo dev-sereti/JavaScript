7c36a10ab84b3c9181ef0d4c0c2d281b60d282aa
dev-sereti <devsereti@gmail.com>
2025-10-07 09:40:47 +0300

    Assign a CSS class for styling

--- Learn Js/To-Do List App/script.js
+++ Learn Js/To-Do List App/script.js
@@ -48,5 +48,6 @@ function createTaskElement(taskText){
    // Create a delete button for removing tasks
    const deleteBtn=document.createElement("delete");
    deleteBtn.textContent="Delete Task"; //The "Delete Task" shown on the button
+   deleteBtn.className="delete"; // Assign a CSS class for styling
    
 }
