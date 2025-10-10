8f76c10f846abef743d16762af387d1fec3a62f3
dev-sereti <devsereti@gmail.com>
2025-10-10 08:54:48 +0300

    create li element

--- Learn Js/To-Do List App/script.js
+++ Learn Js/To-Do List App/script.js
@@ -7,24 +7,7 @@ const listTask=document.getElementById("taskList");
 // ==== CREATE TASK ELEMENT ===
 
 function createTaskElement(textTask){
-  const li=document.createElement("li");
-  li.textContent=inputTask.value.trim();
-
-  li.addEventListener("click",()=>{
-    li.classList.toggle("completed")
-  });
-
-  deleteBtn=document.createElement("button");
-  deleteBtn.textContent="X"
-  deleteBtn.className="delete";
-
-  deleteBtn.addEventListener("click",(e)=>{
-    e.stopPropagation;
-    li.remove;
-    removeTask(textTask);
-  });
-  li.appendChild(deleteBtn);
-  return li;
+const li =document.createElement("li");
 
 }
 // == ADD TASK ===
