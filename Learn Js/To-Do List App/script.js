eab22285efc73e87c95d4895d7dbb46534c83498
dev-sereti <devsereti@gmail.com>
2025-10-14 07:17:09 +0300

    Add the new task.

--- Learn Js/To-Do List App/script.js
+++ Learn Js/To-Do List App/script.js
@@ -54,4 +54,6 @@ function addTask(){
 function saveTask(taskText){
   // Get existing tasks
   let tasks=JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.push(taskText); // Add the new task.
+  localStorage.setItem("tasks",JSON.stringify(tasks));  // Save updated list
 }
