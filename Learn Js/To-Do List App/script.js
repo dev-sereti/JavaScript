904aea64514990da9c38d5bd627989a675f9b0f3
dev-sereti <devsereti@gmail.com>
2025-10-21 10:34:00 +0300

    Add it to the page

--- Learn Js/To-Do List App/script.js
+++ Learn Js/To-Do List App/script.js
@@ -69,6 +69,6 @@ function removeTask(taskText){
   let tasks=JSON.parse(localStorage.getItem("tasks")) || []; //get tasks from localStorage
   tasks.forEach(taskText =>{ // For each task text
     const li= createTaskElement(taskText);  // Build an <li> element
-    listTask.appendChild(li);
+    listTask.appendChild(li); // Add it to the page
   });
  }
