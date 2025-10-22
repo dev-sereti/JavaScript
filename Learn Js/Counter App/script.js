bfeda6b9a4aa02d0aa093a09d51635fb1fed99f7
dev-sereti <devsereti@gmail.com>
2025-10-22 10:59:48 +0300

    declare variables

--- Learn Js/Counter App/script.js
+++ Learn Js/Counter App/script.js
@@ -1,11 +1,13 @@
 // Get HTML elements.
 
-counter=document.getElementById("count");
-incrementBtn=document.getElementById("increment");
-decrementBtn=document.getElementById("decrement");
-resetBtn=document.getElementById("reset");
+const counter=document.getElementById("count");
+const incrementBtn=document.getElementById("increment");
+const decrementBtn=document.getElementById("decrement");
+const resetBtn=document.getElementById("reset");
 
 //Set initial count to 0
 let count=0;
 
 //Increment button clicked.
+
+in
