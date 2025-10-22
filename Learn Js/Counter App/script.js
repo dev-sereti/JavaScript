b6e8bfd91c4b531134eeee29b7ff37dcb945627e
dev-sereti <devsereti@gmail.com>
2025-10-22 10:58:58 +0300

    Set initial count to 0

--- Learn Js/Counter App/script.js
+++ Learn Js/Counter App/script.js
@@ -5,3 +5,7 @@ incrementBtn=document.getElementById("increment");
 decrementBtn=document.getElementById("decrement");
 resetBtn=document.getElementById("reset");
 
+//Set initial count to 0
+let count=0;
+
+//Increment button clicked.
