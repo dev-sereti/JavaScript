f837b0de6c458e569b2ab82c07e600c543e45ac0
dev-sereti <devsereti@gmail.com>
2025-10-22 10:57:25 +0300

    Get HTML elements.

---
+++ Learn Js/Counter App/script.js
@@ -0,0 +1,7 @@
+// Get HTML elements.
+
+counter=document.getElementById("count");
+incrementBtn=document.getElementById("increment");
+decrementBtn=document.getElementById("decrement");
+resetBtn=document.getElementById("reset");
+
