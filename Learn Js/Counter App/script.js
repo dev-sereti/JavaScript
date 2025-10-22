971346ae99f622e9c86970443a4b7cdfefce8ac1
dev-sereti <devsereti@gmail.com>
2025-10-22 10:37:43 +0300

    Start with a value of 0.

--- Learn Js/Counter App/script.js
+++ Learn Js/Counter App/script.js
@@ -4,3 +4,5 @@ const countDisplay=document.getElementById("count");
 const incrementBtn=document.getElementById("increment");
 const decrementBtn=document.getElementById("decrement");
 const resetBtn=document.getElementById("reset");
+
+// Start with a value of 0.
