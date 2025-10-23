2f1a0a04ed86ed7cd2d9a1f1ab34fff0f5b5e4f7
dev-sereti <devsereti@gmail.com>
2025-10-23 10:04:00 +0300

    Add leading zeros to seconds if needed

--- Learn Js/Digital Clock/script.js
+++ Learn Js/Digital Clock/script.js
@@ -14,4 +14,5 @@ function updateClock(){
     // Add leading zeros if needed
     hours= hours < 10 ? "0" + hours:hours;
     minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
 }
