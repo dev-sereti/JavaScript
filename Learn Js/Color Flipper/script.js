07396d5162bd26439531eaa70018c0aa9555563b
dev-sereti <devsereti@gmail.com>
2025-10-24 09:57:18 +0300

    Add event listener to button

--- Learn Js/Color Flipper/script.js
+++ Learn Js/Color Flipper/script.js
@@ -5,3 +5,8 @@ const colors = ["#FF5733", "#33FFBD", "#3EC8F6", "#F3FF33", "#FF33EC", "#8D33FF"
 const btn = document.getElementById("flipBtn");
 const colorCode = document.getElementById("colorCode");
 
+// Add event listener to button
+
+btn.addEventListener("click",()=>{
+
+});
