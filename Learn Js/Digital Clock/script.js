b7c3c018e350f4f358cc1b09b2b965a7dc6772c1
dev-sereti <devsereti@gmail.com>
2025-10-24 08:59:41 +0300

    Add 0 to hrs, min and sec if needed.

--- Learn Js/Digital Clock/script.js
+++ Learn Js/Digital Clock/script.js
@@ -5,4 +5,9 @@ function updateClock(){
 
   // Get date,hours, minutes and seconds.
   const now = new Date();
+
+  // Add 0 to hrs, min and sec if needed.
+  hours = now.getHours;
+  minutes=now.getMinutes;
+  seconds=now.getSeconds;
 }
