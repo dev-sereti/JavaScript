288b371f8f9fdf14c5b3b1162d9de37acf622a9f
dev-sereti <devsereti@gmail.com>
2025-10-28 22:00:55 +0300

    Listen for form submission

--- Learn Js/Form Validator/script.js
+++ Learn Js/Form Validator/script.js
@@ -4,3 +4,9 @@ const nameInput = document.getElementById("name");
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
 
+// Listen for form submission
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault(); 
+
+});
